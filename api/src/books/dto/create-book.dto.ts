191912b7ddd88152de2a2952142fad59d96563b9
Aleum Yang Urfels <aleum.yu@gmail.com>
2025-05-10 23:42:36 -0400

    create reesources: users, books, loans

---
+++ api/src/books/dto/create-book.dto.ts
@@ -0,0 +1,8 @@
+export class CreateBookDto {
+  title: string;
+  author: string;
+  description: string;
+  category: string;
+  publisher: string;
+  year: number;
+}
---
+++ api/src/loans/dto/create-loan.dto.ts
@@ -0,0 +1,4 @@
+export class CreateLoanDto {
+  bookId: string;
+  userId: string;
+}
