44f82b8a97c87a374e26d7d2d89055538c542f8c
Aleum Yang Urfels <aleum.yu@gmail.com>
2025-05-19 14:16:29 -0400

    loan table migration

--- api/src/loans/dto/create-loan.dto.ts
+++ api/src/loans/dto/create-loan.dto.ts
@@ -1,4 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateLoanDto {
   bookId: string;
   userId: string;
 }
+export class UpdateLoanDto extends PartialType(CreateLoanDto) {}
